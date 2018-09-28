1076daf7e52fa136aef0e2b83b4229534ecc1c1b
pradosh kumar rout <pradosh.rout1990@gmail.com>
2018-09-28 10:58:07 +0530

    first commit

---
+++ Core/src/com/pkr/jlc/simpleprogram.java
@@ -0,0 +1,11 @@
+package com.pkr.jlc;
+
+public class simpleprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("i am pkr");
+System.out.println("u r a good boy");
+	}
+
+}
